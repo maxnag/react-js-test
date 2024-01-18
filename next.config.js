e101af17b790de54d1ac7f1e9a4c079f54dad353
Max Nagaychenko <nagaychenko.dev@gmail.com>
2024-01-18 22:26:35 +0200

    Create GitHub Action for testing

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@ const pkg = require('./package.json');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   generateBuildId: async () => {
-    const commitHash = require('child_process')
-      .execSync('git log --pretty=format:"%h" -n1')
-      .toString()
-      .trim();
+    const commitHash = require('child_process').execSync('git log --pretty=format:"%h" -n1').toString().trim();
 
     return `${pkg.version}-${commitHash}`;
   },
